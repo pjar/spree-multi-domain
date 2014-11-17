ae60f731f8b629da5bce90f3671f584505736c01
Jarek Plonski <jarek.github@gmail.com>
2014-11-17 15:50:50 +0100

    fix migrations for spree 2.4.0.rc4

--- db/migrate/20091202123245_add_order_store.rb
+++ db/migrate/20091202123245_add_order_store.rb
@@ -2,7 +2,7 @@ class AddOrderStore < ActiveRecord::Migration
   def self.up
     if table_exists?('orders')
       add_column :orders, :store_id, :integer
-    elsif table_exists?('spree_orders')
+    elsif table_exists?('spree_orders') && !column_exists?(:spree_orders, :store_id, :integer)
       add_column :spree_orders, :store_id, :integer
     end
   end

b2491c7722fb8a17cfa8bf23f89807c55a9b244f
Jarek Plonski <jarek.github@gmail.com>
2015-06-28 23:18:48 +0200

    Show stores column in products index when other plugins override it too

--- app/overrides/decorate_admin_products_index.rb
+++ app/overrides/decorate_admin_products_index.rb
@@ -3,6 +3,7 @@ Deface::Override.new(
   :name => "multi_domain_admin_products_index_headers",
   :insert_before => "[data-hook='admin_products_index_header_actions']",
   :partial => "spree/admin/products/index_headers",
+  :sequence => 1000,
   :disabled => false)
 
 Deface::Override.new(
@@ -10,6 +11,7 @@ Deface::Override.new(
   :name => "multi_domain_admin_products_index_rows",
   :insert_before => "[data-hook='admin_products_index_row_actions']",
   :partial => "spree/admin/products/index_rows",
+  :sequence => 1001,
   :disabled => false)
 
 Deface::Override.new(
@@ -17,4 +19,5 @@ Deface::Override.new(
   :name => "multi_domain_admin_products_index_search",
   :insert_top => "[data-hook='admin_products_index_search']",
   :partial => "spree/admin/products/index_search_fields",
+  :sequence => 1002,
   :disabled => false)
